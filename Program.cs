cd6c8188bf4f9834b7ab0c32e9e6eec71b124978
Diogo Ribeiro <ribeirodiogo735@gmail.com>
2021-04-15 11:08:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+
+
+namespace DiscordBotDM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            var token = File.ReadAllText("token.txt");
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+            return Task.CompletedTask;
+        }
+        [Command("DM")]
+        public async Task DmUser(ulong id, string message)
+        {
+            message = "pong";
+            var dm = _client.GetUser(id);
+            await dm.SendMessageAsync(message);
+        }
+
+    }
+
+}
